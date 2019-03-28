43091192fd88d5751aab5ce163ac393b07860f4b
Kyle Kendall <kkendall33@gmail.com>
2019-03-27 18:44:28 -0600

    Add swift 5

--- NetworkTests/NetworkTests.swift
+++ NetworkTests/NetworkTests.swift
@@ -9,6 +9,16 @@
 import XCTest
 @testable import NetworkRequest
 
+private let domain = "carswaddle.com"
+
+public let serviceRequest: Request = {
+    let request = Request(domain: domain)
+    request.port = 3000
+    request.timeout = 15
+    request.defaultScheme = .http
+    return request
+}()
+
 class NetworkTests: XCTestCase {
 
     func testPerformanceExample() {
@@ -19,6 +29,16 @@ class NetworkTests: XCTestCase {
             XCTAssert(false, "Error with path ")
         }
         
+    }
+    
+    func multipartFormRequest() {
+        guard let fileURL = Bundle(for: type(of: self)).url(forResource: "image", withExtension: "png") else {
+            XCTAssert(false, "Should have file: image.jpeg in test bundle")
+            return
+        }
+        
+        let request = serviceRequest.multipartFormDataPost(withPath: "/api/profile-picture")
+        serviceRequest.uploadMultipartFormDataTask(with: request, fileURL: fileURL, contentType: <#T##String#>, completion: <#T##(Data?, HTTPURLResponse?, Error?) -> Void#>)
     }
     
 }
